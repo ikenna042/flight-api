4b44d20bbb268cabadf947fc75425a3e43969c12
IkennaVictor <ikennavictorofficial@gmail.com>
2019-10-31 23:51:11 +0100

    ran apidoc

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "FLIGHT API v1",
+  "version": "0.1.0",
+  "description": "API for flight management",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-10-31T22:50:25.341Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
